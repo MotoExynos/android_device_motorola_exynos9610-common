d944fbf9994bd7b17db5632ecc6c984b702f86b5
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    exynos9610: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- common.mk
+++ common.mk
@@ -66,7 +66,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     libldacBT_bco \
     libbt-vendor
