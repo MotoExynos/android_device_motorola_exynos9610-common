8573b93a21e3fd2c0a2e6b627eaf6332c7ccb521
The Vision <x@zlew.tf>
2022-11-16 16:12:55 +0100

    exynos9610: Drop livedisplay for normal edition builds
    Signed-off-by: The Vision <x@zlew.tf>
    Change-Id: I706e9cd010486a0b88c76ebe61e21855826266df

--- common.mk
+++ common.mk
@@ -194,10 +194,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.samsung
 
-# Livedisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.exynos9610
-
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
